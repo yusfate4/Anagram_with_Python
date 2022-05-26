c2326fe969cbac99c4578126d0b3fb79c63d9ba4
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-05-26 12:33:35 +0100

    This code finds the Anagrams between two words

---
+++ main.py
@@ -0,0 +1,14 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+def find_anagram(word, anagram):
+    word = sorted(word)
+    anagram = sorted(anagram)
+
+    return word == anagram
+
+    
+print(find_anagram ("hello", "check"))
+print(find_anagram ("below", "elbow"))
